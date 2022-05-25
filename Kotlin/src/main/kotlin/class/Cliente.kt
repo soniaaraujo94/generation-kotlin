066d71e016b1f7940cc86b4acd09dfd4d9d43c66
Sonia Araújo <so.araujo94@gmail.com>
2022-05-24 21:40:16 -0300

    pooAdvanced

---
+++ Kotlin/src/main/kotlin/class/Cliente.kt
@@ -0,0 +1,50 @@
+class Cliente(private var nome: String) {
+
+    var endereco = ""
+    var telefone = ""
+    var listaDeCompras = mutableListOf<String>()
+
+    constructor(nome: String, endereco: String, telefone: String)
+    :this(nome){
+        this.endereco = endereco
+        this.telefone = telefone
+    }
+
+    fun addCompra () {
+        println("\n--- Adicionar produto ---")
+        println("Qual produto você quer comprar?")
+        val produto = readln()
+
+        if (produto != ""){
+
+            listaDeCompras.add(produto)
+
+        }else{
+            println("Produto Inválido")
+        }
+
+    }
+
+    fun removeCompra () {
+        println("\n--- Remover produto ---")
+        println("Qual produto você quer remover da sua lista de compras?")
+        val produto = readln()
+
+        if (produto != "" && listaDeCompras.contains(produto)){
+
+            listaDeCompras.remove(produto)
+
+        }else {
+            println("Produto Inválido")
+        }
+    }
+
+    fun listCompra () {
+
+        println()
+
+        listaDeCompras.forEach {
+            println(it)
+        }
+    }
+}
---
+++ Kotlin/src/main/kotlin/exCliente.kt
@@ -0,0 +1,36 @@
+fun main(){
+
+/*
+Cria uma Classe Cliente, contendo os atributos encapsulados, um construtor padrão
+e pelo menos mais duas opções de construtores conforme sua percepção,
+com os atributos: nome (String), endereço (String), telefone (String), listaDeCompras
+(mutableListOf<Strings>()).
+
+A classe precisará de métodos para adicionar, remover e listar os itens do atributo
+listaDeCompras. Crie um método para verificar o nome ao inicializar a classe e,
+caso esteja vazio, jogue uma exceção. Ao instanciar a classe cliente na
+função main(), não esquecer de colocar dentro de um bloco try catch.
+ */
+
+val cliente = Cliente(
+    "Rodrigo","Rua Santa 324","110000-0000"
+    )
+
+    while (true) {
+        println(
+            "\n1) Adicionar Item" +
+                    "\n2) Remover Item" +
+                    "\n3) Listar Itens" +
+                    "\n4) Sair"
+        )
+        var compraEscolha = readln().toInt()
+
+        when (compraEscolha) {
+
+            1 -> cliente.addCompra()
+            2 -> cliente.removeCompra()
+            3 -> cliente.listCompra()
+            4 -> break
+        }
+    }
+}
