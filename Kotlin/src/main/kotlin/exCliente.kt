07ce423f50d599cfdb430e1515c6860564e14b1f
Sonia Araújo <so.araujo94@gmail.com>
2022-05-24 21:51:41 -0300

    tryCatch

--- Kotlin/src/main/kotlin/exCliente.kt
+++ Kotlin/src/main/kotlin/exCliente.kt
@@ -1,4 +1,4 @@
-fun main(){
+fun main() {
 
 /*
 Cria uma Classe Cliente, contendo os atributos encapsulados, um construtor padrão
@@ -12,25 +12,30 @@ caso esteja vazio, jogue uma exceção. Ao instanciar a classe cliente na
 função main(), não esquecer de colocar dentro de um bloco try catch.
  */
 
-val cliente = Cliente(
-    "Rodrigo","Rua Santa 324","110000-0000"
-    )
-
-    while (true) {
-        println(
-            "\n1) Adicionar Item" +
-                    "\n2) Remover Item" +
-                    "\n3) Listar Itens" +
-                    "\n4) Sair"
+    try {
+        val cliente = Cliente(
+            "Rodrigo", "Rua Santa 324", "110000-0000"
         )
-        var compraEscolha = readln().toInt()
 
-        when (compraEscolha) {
+        while (true) {
+            println(
+                "\n1) Adicionar Item" +
+                        "\n2) Remover Item" +
+                        "\n3) Listar Itens" +
+                        "\n4) Sair"
+            )
+            var compraEscolha = readln().toInt()
 
-            1 -> cliente.addCompra()
-            2 -> cliente.removeCompra()
-            3 -> cliente.listCompra()
-            4 -> break
+            when (compraEscolha) {
+
+                1 -> cliente.addCompra()
+                2 -> cliente.removeCompra()
+                3 -> cliente.listCompra()
+                4 -> break
+            }
         }
+
+    } catch (e: Exception) {
+        println(e.message)
     }
 }
